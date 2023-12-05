fc0a4585c64da0fe20c4b2f515dfa1960fe9ed30
Nicegreanz <70066609+Nicegreanz@users.noreply.github.com>
2023-12-05 23:12:46 +0700

    Add files via upload

---
+++ Exercise5_1_Jutichot_P.py
@@ -0,0 +1,6 @@
+n1=int(input("First number :"))
+n2=int(input("Second number :"))
+print(f"{n1} + {n2} = {n1+n2}")
+print(f"{n1} - {n2} = {n1-n2}")
+print(f"{n1} * {n2} = {n1*n2}")
+print(f"{n1} / {n2} = {n1/n2}")
